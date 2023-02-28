a6f1a022e1515a94353e5ca5efbdb021a1c0bd52
FilipRazek <65445960+FilipRazek@users.noreply.github.com>
2023-02-28 10:44:00 +0100

    :recycle: Move to Playwright Crawler

--- src/pro-crawler.ts
+++ src/pro-crawler.ts
@@ -1,19 +1,27 @@
-import { CheerioCrawler, Dataset } from "crawlee";
+import { PlaywrightCrawler, Dataset } from "crawlee";
+import { load } from "cheerio";
 
-const crawler = new CheerioCrawler({
-  requestHandler: async ({ $, request, enqueueLinks }) => {
+const crawler = new PlaywrightCrawler({
+  requestHandler: async ({ page, request, enqueueLinks }) => {
+    const $ = load(await page.content());
+
     if (request.label === "START") {
       await enqueueLinks({
         selector: 'a[href*="/product/"]',
         baseUrl: new URL(request.url).origin,
       });
+      return;
     }
 
     const title = $("h3").text().trim();
     const price = $("h3 + div").text().trim();
     const description = $('div[class*="Text_body"]').text().trim();
 
-    await Dataset.pushData({ title, description, price });
+    const imageRelative = $('img[alt="Product Image"]').attr("src");
+    const base = new URL(request.url).origin;
+    const image = new URL(imageRelative, base).href;
+
+    await Dataset.pushData({ title, description, price, image });
   },
 });
 
